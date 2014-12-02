5044fb65e4a7fd11dcfe8d2fe98db7dad666b62f
sharmis11 <ss5qu@virginia.edu>
2014-12-02 13:56:01 -0500

    modified:   NLCE.py

--- NLCE.py
+++ NLCE.py
@@ -9,43 +9,44 @@ from mxn_order import *
 # User settings
 
 order_min = 2
-order_max = 5
+order_max = 36 
 order_step = 1
 order = Arithmetic()
-filename = "Results"
-print "Writing file ",filename
-f = open(filename, 'a')
 #############################
-#for r in range(1,5):
-r = 1 #radius 4
-total = None
-w = {} # weights
-missing = [] # The list of missing data files
+for r in range(1,3):
+    #r = 1 #radius 4
+    R=4*r
+    filename = "Results_%02d"%(R,)
+    print "Writing file ",filename
+    f = open(filename, 'a')
+    total = None
+    w = {} # weights
+    missing = [] # The list of missing data files
 
-clusters = []
+    clusters = []
 
-for I in frange(order_min,order_max+0.01,order_step):
-    for m,n in order.clusters(I):
-        filename= "Database/"+"%02d_%02d"%(m,n)
-        try:
-            db = dbm.open(filename)
-            db.close()
-        except dbm.error:
-            print "Can't open %s" %(filename)
-            missing.append(filename)
+    for I in frange(order_min,order_max+0.01,order_step):
+        for m,n in order.clusters(I):
+            filename= "Database/"+"%02d_%02d"%(m,n)
+            try:
+                db = dbm.open(filename)
+                db.close()
+            except dbm.error:
+                print "Can't open %s" %(filename)
+                missing.append(filename)
 
-        if len(missing) == 0:
-            w = mxn_weight.weight(m,n,r,w) # performs cluster weight calculations
+            if len(missing) == 0:
+                w = mxn_weight.weight(m,n,r,w) # performs cluster weight calculations
 
             #Embedding factor (1 for squares, 2 for rectangles):
-            Lc = 1
-            #if m != n: Lc = 2   #see line 24, mxn_weight.py
+                Lc = 1
+                if m != n: Lc = 2   #see line 24, mxn_weight.py
 
             # cannot use total += w['%02d%02d'%(m,n)] or else W0202 somehow gets changed every iteration
-            if total is None:
-                total = Lc*w['%02d%02d'%(m,n)]
-            else:
-                total = total + Lc*w['%02d%02d'%(m,n)]
+                if total is None:
+                    total = Lc*w['%02d%02d'%(m,n)]
+                else:
+                    total = total + Lc*w['%02d%02d'%(m,n)]
 
         # Save result to file
         f.write("%d %.15f"%(I,total)+'\n')
@@ -60,4 +61,4 @@ for I in frange(order_min,order_max+0.01,order_step):
         print "The following data files were not found:"
         for m in missing:
             print "  ",m
-f.close()
+    f.close()
