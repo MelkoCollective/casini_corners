56b815bfb1bbfa8546e891c518974ba18f4a8ad6
rgmelko <rgmelko@uwaterloo.ca>
2014-12-05 13:58:12 -0500

    fixed empty database prob

--- NLCE.py
+++ NLCE.py
@@ -37,17 +37,17 @@ for I in frange(order_min,order_max+0.01,order_step):
             print "Can't open %s" %(filename)
             missing.append(filename)
 
-        if len(missing) == 0:
-            w = mxn_weight.weight(m,n,R,w) # performs cluster weight calculations
+        #if len(missing) == 0:
+        w = mxn_weight.weight(m,n,R,w) # performs cluster weight calculations
 
         #Embedding factor (1 for squares, 2 for rectangles):
-            Lc = 1
-            if m != n: Lc = 2   #see line 24, mxn_weight.py
-            # cannot use total += w['%02d%02d'%(m,n)] or else W0202 somehow gets changed every iteration
-            total = total + Lc*w['%02d%02d'%(m,n)]
+        Lc = 1
+        if m != n: Lc = 2   #see line 24, mxn_weight.py
+        # cannot use total += w['%02d%02d'%(m,n)] or else W0202 somehow gets changed every iteration
+        total = total + Lc*w['%02d%02d'%(m,n)]
 
     # Save result to file
-    #f.write("%d %.15f"%(I,total)+'\n')
+    f.write("%d %.15f"%(I,total)+'\n')
 
 # Show all required data files
 #print "The following data files are required:"
@@ -55,8 +55,8 @@ for I in frange(order_min,order_max+0.01,order_step):
 #    print "  ",r 
 
 # If any missing data
-    if len(missing) > 0:
-        print "The following data files were not found:"
-        for m in missing:
-            print "  ",m
-    f.close()
+if len(missing) > 0:
+   print "The following data files were not found:"
+   for m in missing:
+       print "  ",m
+f.close()
