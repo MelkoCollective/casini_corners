ad92085cb48eadb1bc818e13d9978afd672bd588
sharmis11 <ss5qu@virginia.edu>
2014-12-10 11:44:42 -0500

    added file to calculate entropy from intercept

---
+++ entropyfromfit.py
@@ -0,0 +1,45 @@
+import numpy as np
+from numpy import matrix,linalg,mean
+from math import log,sqrt
+import sys
+
+lmax = 35
+lmin = 3
+lstep = 1
+
+results_template = "Results_circle_%02d"
+
+def linfit(x_list,y_list): #includes linear fitting as a special case
+    Y = matrix([[y_] for y_ in y_list])
+    X = matrix([[x_,1] for x_ in x_list])
+
+    (Q,R) = linalg.qr(X)
+
+    (m,b) = [float(item) for item in linalg.solve(R,Q.transpose()*Y)]
+
+    rms_err = sqrt(mean([float(item)**2 for item in Y-X*matrix([[m],[b]])]))
+
+    return (m,b,rms_err)
+
+entropy = []
+for i in range (1,9):
+    R=2*i
+    fname = results_template%(R,)
+    f = open(fname, 'r')
+    invorder = []
+    EE = []
+    for lines in f.readlines()[8:]:
+        line = lines.split()
+        invorder.append(1/float(line[0]))
+        EE.append(float(line[1]))
+    f.close()
+    print EE,invorder
+    m,b,err = linfit(invorder,EE)
+    entropy.append(b)
+
+f = open("entropy_circle",'w')
+for i in range (1,9):
+    R = 2*i
+    f.write("%02d %.20f\n"%(R,entropy[i-1]))
+f.close()
+
